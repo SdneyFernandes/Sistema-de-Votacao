5c53c9994cb037501d3fa46a23eda10cd6a47cf9
SdneyFernandes <fsidney987@gmail.com>
2025-02-25 02:51:23 -0300

    criando entidades e mapeando as tabelas de Vote, Candidate, Election no microsserviço de Votes

---
+++ voting-system-vote-service/src/main/java/br/com/voting_system_vote_service/entity/Candidate.java
@@ -0,0 +1,31 @@
+package br.com.voting_system_vote_service.entity;
+
+import jakarta.persistence. *;
+import lombok. *;
+
+/**
+ * @author fsdney
+ */
+
+@Entity
+@Table(name = "table_candidate")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Candidate {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long ig;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "election_id", nullable = false)
+	private Election election;
+	
+	private int votes; //contador de votos recebidos
+}
---
+++ voting-system-vote-service/src/main/java/br/com/voting_system_vote_service/entity/Vote.java
@@ -0,0 +1,38 @@
+package br.com.voting_system_vote_service.entity;
+
+import jakarta.persistence. *;
+import lombok. *;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author fsdney
+ */
+
+@Entity
+@Table(name = "table_vote")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vote {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Long userId;
+	
+	@ManyToOne
+	@JoinColumn(name = "candidate_id", nullable = false)
+	private Candidate candidate;
+	
+	@ManyToOne
+	@JoinColumn(name = "election_id", nullable = false)
+	private Election election;
+	
+	@Column(nullable = false) 
+	private LocalDateTime timesstamp;
+}
