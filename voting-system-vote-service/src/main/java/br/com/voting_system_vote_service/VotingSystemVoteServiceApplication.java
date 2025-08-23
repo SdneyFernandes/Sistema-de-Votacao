585b9d3167a111f1b81cb46dc0553d23972e80ca
SdneyFernandes <fsidney987@gmail.com>
2025-08-23 20:06:30 -0300

    main

--- voting-system-vote-service/src/main/java/br/com/voting_system_vote_service/VotingSystemVoteServiceApplication.java
+++ voting-system-vote-service/src/main/java/br/com/voting_system_vote_service/VotingSystemVoteServiceApplication.java
@@ -3,10 +3,8 @@ package br.com.voting_system_vote_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;;
 
-@EnableDiscoveryClient
-@EnableFeignClients
+@EnableDiscoveryClient  
 @SpringBootApplication
 public class VotingSystemVoteServiceApplication {
 
--- voting_service_frontEnd/src/pages/Home/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const HomeWrapper = styled.div`
-  padding: 20px;
-`
-
-export const Title = styled.h2`
-  color: ${({ theme }) => theme.colors.secondary};
-`
