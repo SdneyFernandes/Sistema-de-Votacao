5d3f5741e3c216653f7907a0d463588b7bca8e2c
SdneyFernandes <fsidney987@gmail.com>
2025-08-27 02:09:20 -0300

    e

--- voting-system-api-gateway/src/main/java/br/com/voting_system_api_gateway/config/GatewayConfig.java
+++ voting-system-api-gateway/src/main/java/br/com/voting_system_api_gateway/config/GatewayConfig.java
@@ -4,8 +4,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import br.com.voting_system_api_gateway.filter.CookieAuthenticationFilter; // Importante!
-
+import br.com.voting_system_api_gateway.filter.CookieAuthenticationFilter;
 
 @Configuration
 public class GatewayConfig {
@@ -19,11 +18,11 @@ public class GatewayConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, CookieAuthenticationFilter cookieAuthenticationFilter) {
         return builder.routes()
                 .route("user-service", r -> r.path("/api/users/**")
-                        .filters(f -> f.filter(cookieAuthenticationFilter)) // Adicione o filtro
-                        .uri("lb://voting-system-user-service"))
+                        .filters(f -> f.filter(cookieAuthenticationFilter))
+                        .uri("https://voting-system-user-service.onrender.com"))
                 .route("vote-service", r -> r.path("/api/votes/**", "/api/votes_session/**")
-                        .filters(f -> f.filter(cookieAuthenticationFilter)) // Adicione o filtro
-                        .uri("lb://voting-system-vote-service"))
+                        .filters(f -> f.filter(cookieAuthenticationFilter))
+                        .uri("https://voting-system-vote-service.onrender.com"))
                 .build();
     }
 }
